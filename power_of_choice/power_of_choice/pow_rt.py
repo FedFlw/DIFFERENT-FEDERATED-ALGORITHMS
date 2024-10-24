d8d3226cad01fbf7b7fa8f335aad411d1d42dc5a
FedFlw <iyad.wehbe@polimi.it>
2024-10-24 16:19:43 +0200

    Update pow_rt.py

--- power_of_choice/power_of_choice/pow_rt.py
+++ power_of_choice/power_of_choice/pow_rt.py
@@ -24,7 +24,7 @@ from omegaconf import DictConfig, OmegaConf
 from strategy import RTFedAvg
 
 from power_of_choice.client_rt import gen_client_fn
-from power_of_choice.models import create_CNN_model, create_MLP_model
+from power_of_choice.models import  create_MLP_model
 from power_of_choice.server import PowerOfChoiceCommAndCompVariant, PowerOfChoiceServer
 from power_of_choice.utils import save_results_as_pickle
 
@@ -54,7 +54,7 @@ def main(cfg: DictConfig) -> None:
     # 3. Define your clients
     # Define a function that returns another function that will be used during
     # simulation to instantiate each individual client
-    client_fn = gen_client_fn(cfg.client.mean_ips, cfg.client.var_ips, cfg.num_clients, cfg.is_cnn)
+    client_fn = gen_client_fn(cfg.client.mean_ips, cfg.client.var_ips, cfg.num_clients)
 
     # 4. Define your strategy
     # pass all relevant argument (including the global dataset used after aggregation,
@@ -121,9 +121,7 @@ def main(cfg: DictConfig) -> None:
         print(f"Current folder is {os.getcwd()}")
 
         test_folder = "mnist"
-        if cfg.is_cnn:
-            test_folder = "cifar10"
-
+        
         # Load data and model here to avoid the overhead of doing it in `evaluate`
         x_test = np.load(os.path.join(test_folder, "x_test.npy"))
         y_test = np.load(os.path.join(test_folder, "y_test.npy"))
@@ -140,10 +138,7 @@ def main(cfg: DictConfig) -> None:
 
         return evaluate
 
-    if cfg.is_cnn:
-        server_model = create_CNN_model()
-    else:
-        server_model = create_MLP_model()
+    server_model = create_MLP_model()
 
     server_model.compile(
         "adam", "sparse_categorical_crossentropy", metrics=["accuracy"]
